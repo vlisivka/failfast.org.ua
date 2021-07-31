8dc015c39357e9fa41412f44d41126d1bc6c5cd0
Volodymyr M. Lisivka <vlisivka@gmail.com>
2021-08-01 00:36:26 +0300

    Design updated. Support for kroki is added.

--- src/components/BlogTeaser.svelte
+++ src/components/BlogTeaser.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <div style="width:300px; padding: 1em; max-width:300px;">
-<Card>
+<Card style="height:100%">
   <div slot="thumb"><img src={blog.frontmatter.thumb} alt="Thumbnail for blog post" style="width: 100%"/></div>
   <div slot="header"><h4><a href={helpers.permalinks.blog({ slug: blog.slug })}>{blog.frontmatter.title}</a></h4></div>
   <p>{blog.frontmatter.excerpt}</p>
