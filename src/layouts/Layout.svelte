9e45bf37bc529e2d139e33e9deeebd35f723ab5f
Volodymyr M. Lisivka <vlisivka@gmail.com>
2021-08-01 17:45:17 +0300

    Favicon is added in SVG.

--- src/layouts/Layout.svelte
+++ src/layouts/Layout.svelte
@@ -36,6 +36,9 @@
   }
 </style>
 
+<svelte:head>
+<link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 120 100%22><text y=%22.9em%22 font-size=%2290%22>FF</text></svg>">
+</svelte:head>
 <div class="container">
   {@html templateHtml}
 </div>
